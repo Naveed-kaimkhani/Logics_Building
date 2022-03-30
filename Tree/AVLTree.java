8f72a59bd2fab97d80e13f8483e05e514dafd1d1
Naveed kaimkhani <naveedkaimkhami@gmail.com>
2022-03-30 10:46:07 +0500

    fsd

--- Tree/AVLTree.java
+++ Tree/AVLTree.java
@@ -1,3 +1,5 @@
+//make a tree
+
 public class AVLTree {
     private int key,height;
     private AVLTree left,right;
@@ -12,4 +14,39 @@ public class AVLTree {
         this.right=right;
         height =1+Math.max(left.height, right.height);
     }
+    public int size(){
+        if (this==NIL) return 0;
+        return 1+left.size()+right.size();
+    }
+    public String toString(){
+        if(this==NIL)return " ";
+        return left+" "+key+" "+right;
+    }
+    public boolean add(int key) {
+        int oldsize=size();
+        grow(key);
+        return size()>oldsize;
+    }
+public AVLTree grow(int key){
+    if (this=NIL) {
+     return new AVLTree(key);   
+    }
+    if(key==this.key)return this;
+    if(key<this.key)left=left.grow(key);
+    else right=right.grow(key);
+    rebalance();
+    height=1+Math.max(left.height, right.height);
+    return this;
+}
+private void rotateleft(){
+    left=new AVLTree(key,left,right.left);
+    key=right.key;
+    //right=right.key;
+    right=right.right;
+}
+private void rotateRight(){
+    right=new AVLTree(key,left.right,right);
+    key=left.key;
+    left=left.left;
+}
 }
