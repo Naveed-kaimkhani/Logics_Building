74130e5086c84d36193509ca31d00d24965808fb
Naveed kaimkhani <naveedkaimkhami@gmail.com>
2022-04-09 00:13:32 +0500

    fsd

--- Tree/AVLTree.java
+++ Tree/AVLTree.java
@@ -1,52 +1,64 @@
-//make a tree
 
 public class AVLTree {
-    private int key,height;
-    private AVLTree left,right;
-    public static final AVLTree NIL=new AVLTree();
-    private AVLTree (){
-        left=right=this;
-        height=-1;
+    private int key, height;
+    private AVLTree left, right;
+    public static final AVLTree NIL = new AVLTree();
+
+    private AVLTree() {
+        left = right = this;
+        height = -1;
     }
-    private AVLTree (int key, AVLTree left,AVLTree right){
-        this.key=key;
-        this.left=left;
-        this.right=right;
-        height =1+Math.max(left.height, right.height);
+
+    private AVLTree(int key, AVLTree left, AVLTree right) {
+        this.key = key;
+        this.left = left;
+        this.right = right;
+        height = 1 + Math.max(left.height, right.height);
     }
-    public int size(){
-        if (this==NIL) return 0;
-        return 1+left.size()+right.size();
+
+    public int size() {
+        if (this == NIL)
+            return 0;
+        return 1 + left.size() + right.size();
     }
-    public String toString(){
-        if(this==NIL)return " ";
-        return left+" "+key+" "+right;
+
+    public String toString() {
+        if (this == NIL)
+            return " ";
+        return left + " " + key + " " + right;
     }
+
     public boolean add(int key) {
-        int oldsize=size();
+        int oldsize = size();
         grow(key);
-        return size()>oldsize;
+        return size() > oldsize;
     }
-public AVLTree grow(int key){
-    if (this==NIL) {
-     return new AVLTree(key);   
+
+    public AVLTree grow(int key) {
+        if (this == NIL) {
+            return new AVLTree(key);
+        }
+        if (key == this.key)
+            return this;
+        if (key < this.key)
+            left = left.grow(key);
+        else
+            right = right.grow(key);
+        rebalance();
+        height = 1 + Math.max(left.height, right.height);
+        return this;
     }
-    if(key==this.key)return this;
-    if(key<this.key)left=left.grow(key);
-    else right=right.grow(key);
-    rebalance();
-    height=1+Math.max(left.height, right.height);
-    return this;
-}
-private void rotateleft(){
-    left=new AVLTree(key,left,right.left);
-    key=right.key;
-    //right=right.key;
-    right=right.right;
-}
-private void rotateRight(){
-    right=new AVLTree(key,left.right,right);
-    key=left.key;
-    left=left.left;
-}
+
+    private void rotateleft() {
+        left = new AVLTree(key, left, right.left);
+        key = right.key;
+        // right=right.key;
+        right = right.right;
+    }
+
+    private void rotateRight() {
+        right = new AVLTree(key, left.right, right);
+        key = left.key;
+        left = left.left;
+    }
 }
